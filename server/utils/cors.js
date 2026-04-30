02acbbe89345dbc8cd127d73b859e3f4b7fed722
Muhammed Shafi SR <shafi.sd@webaxium.com>
2026-04-30 12:23:16 +0530

    fix: update CORS for digital ocean

--- server/utils/cors.js
+++ server/utils/cors.js
@@ -7,10 +7,16 @@ const configureCors = () =>
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
 
-      const allowed = allowedOrigins?.map((o) => o?.replace(/\/$/, ""));
+      const allowed = [
+        process.env.CLIENT_URL,
+        "https://six-a-skillcity-m25sx.ondigitalocean.app"
+      ].map((o) => o?.replace(/\/$/, ""));
+      
       const clean = origin?.replace(/\/$/, "");
 
-      if (allowed.includes(clean)) return callback(null, true);
+      if (allowed.includes(clean) || clean.endsWith(".ondigitalocean.app")) {
+        return callback(null, true);
+      }
 
       console.log("Blocked CORS:", origin);
       callback(new Error("Not allowed by CORS"));
